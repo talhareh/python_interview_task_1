33d2da94dfa9fe27cc01d86ca261c337807360c1
fathimashihab <fathimashihab4810@gmail.com>
2023-10-26 18:34:31 +0530

    updated API

--- imagedisplay/imageapi/views.py
+++ imagedisplay/imageapi/views.py
@@ -1,29 +1,22 @@
 
 
-# Create your views here.
-from django.shortcuts import render, redirect
-from .models import ImageUploadForm
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
 import os
 
-def image_list(request):
-    image_dir = os.path.join(os.getcwd(), 'media/images')
-    images = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
-    return render(request, 'src/imagelist.js', {'images': images})
-
 def upload_image(request):
-    if request.method == 'POST':
-        form = ImageUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            image = request.FILES['image']
-            
-            # Save image to media folder
-            image_dir = os.path.join(os.getcwd(), 'media/images')
-            with open(os.path.join(image_dir, image.name), 'wb') as destination:
-                for chunk in image.chunks():
-                    destination.write(chunk)
+    if request.method == 'POST' and request.FILES['image']:
+        image = request.FILES['image']
+        title = request.POST.get('title', '')
+        with open(os.path.join(settings.MEDIA_ROOT, image.name), 'wb') as f:
+            for chunk in image.chunks():
+                f.write(chunk)
+        return JsonResponse({'message': 'Image uploaded successfully'})
+    return JsonResponse({'message': 'Image upload failed'})
 
-            return redirect('image_list')
-    else:
-        form = ImageUploadForm()
-    return render(request, 'src/upload.js', {'form': form})
+def list_images(request):
+    media_path = settings.MEDIA_ROOT
+    image_files = [f for f in os.listdir(media_path) if os.path.isfile(os.path.join(media_path, f))]
+    images = [{'title': os.path.splitext(image)[0], 'url': os.path.join(settings.MEDIA_URL, image)} for image in image_files]
+    return JsonResponse(images, safe=False)
